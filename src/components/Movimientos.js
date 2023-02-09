2c4f2edb896c549010cf131b804df715b17055c5
PabloSerratti <dev05@digipar.com>
2023-02-09 15:34:00 -0300

    Ajustes en el form de movimiento

--- src/components/Movimientos.js
+++ src/components/Movimientos.js
@@ -24,72 +24,94 @@ const Movimientos = () => {
     ];
 
     const [alert, setAlert] = React.useState(false);
-    const [movimientos, setMovimientos] = React.useState([]);
+    const [movimientos, setMovimientos] = React.useState(
+        [
+            {
+                "Id" : "001",
+                "Creado" : "2023-02-01T12:53:28Z",
+                "CreadoPor" : "Pedro Kaur",
+                "Modificado" : "2023-02-01T12:53:28Z",
+                "ModificadoPor": "Pedro Kaur",
+                "Estado" : 1,
+                "Precio" : 800,
+                "Cantidad": 2,
+                "ArticuloId" : "m2o94",
+                "ClienteId" : 1000    },
+            {
+                "Id" : "002",
+                "Creado" : "2023-01-12T12:53:28Z",
+                "CreadoPor" : "Angelina Alfaro",
+                "Modificado" : "2023-01-12T12:53:28Z",
+                "ModificadoPor": "Angelina Alfaro",
+                "Estado" : 1,
+                "Precio" : 900,
+                "Cantidad": 2,
+                "ArticuloId" : "h9kc6",
+                "ClienteId" : 1001    },
+            {
+                "Id" : "003",
+                "Creado" : "2023-01-05T12:53:28Z",
+                "CreadoPor" : "Pedro Kaur",
+                "Modificado" : "2023-01-05T12:53:28Z",
+                "ModificadoPor": "Pedro Kaur",
+                "Estado" : 2,
+                "Precio" : 1200,
+                "Cantidad": 1,
+                "ArticuloId" : "dxhr23",
+                "ClienteId" : 1002    },
+            {
+                "Id" : "004",
+                "Creado" : "2023-02-10T12:53:28Z",
+                "CreadoPor" : "Mohamed Montenegro",
+                "Modificado" : "2023-02-10T12:53:28Z",
+                "ModificadoPor": "Mohamed Montenegro",
+                "Estado" : 2,
+                "Precio" : 1200,
+                "Cantidad": 1,
+                "ArticuloId" : "gft291",
+                "ClienteId" : 1003    },
+            {
+                "Id" : "005",
+                "Creado" : "2023-03-11T12:53:28Z",
+                "CreadoPor" : "Nestor Jurado",
+                "Modificado" : "2023-03-11T12:53:28Z",
+                "ModificadoPor": "Nestor Jurado",
+                "Estado" : 2,
+                "Precio" : 2000,
+                "Cantidad": 0,
+                "ArticuloId" : "krw462",
+                "ClienteId" : 1004    }
+        ]
+    );
     const [alertOptions, setAlertOptions] = React.useState({});
     const { fetchData: fetchMovimientos, error: errorMovimientos, loading: loadingMovimientos } = useFetch();
 
 
-    // const getMovimientos = React.useCallback(async () => {
-    //     const reqOptions = {
-    //         method: 'GET',
-    //         headers: { "Content-Type": "application/json" }
-    //     };
-
-    //     const movimientoData = await fetchMovimientos(`${API}/movimientos`, reqOptions)
-    //     if (movimientoData.error) {
-    //         setAlert(true);
-    //         setAlertOptions({ tipo: 'error', titulo: 'Error', mensaje: movimientoData.message })
-    //     } else if (errorMovimientos) {
-    //         setAlert(true);
-    //         setAlertOptions({ tipo: 'error', titulo: 'Error', mensaje: errorMovimientos })
-    //     } else {
-    //         setArticulos(movimientoData)
-    //     }
-    // }, [errorMovimientos, fetchMovimientos]);
-
-
-    React.useEffect(() => {
-        const getMovimientos = async () => {
-            const reqOptions = {
-                        method: 'GET',
-                        headers: { "Content-Type": "application/json" }
-                    };
-                    const movimientoData = await fetchMovimientos(`${API}/movimientos`, reqOptions)
-                    if (movimientoData.error) {
-                        setAlert(true);
-                        setAlertOptions({ tipo: 'error', titulo: 'Error', mensaje: movimientoData.message })
-                    } 
-                    if (errorMovimientos) {
-                        setAlert(true);
-                        setAlertOptions({ tipo: 'error', titulo: 'Error', mensaje: errorMovimientos })
-                    } 
-                    setMovimientos(movimientoData)
-                    
-        }
-        getMovimientos();
-    }, []) //! fetchMovimientos ciclo infinito
-
-
-    const refreshMovimientos = async () => {
+    const getMovimientos = React.useCallback(async () => {
         const reqOptions = {
             method: 'GET',
             headers: { "Content-Type": "application/json" }
         };
+
         const movimientoData = await fetchMovimientos(`${API}/movimientos`, reqOptions)
 
         if (movimientoData.error) {
             setAlert(true);
             setAlertOptions({ tipo: 'error', titulo: 'Error', mensaje: movimientoData.message })
-        }
+        } 
         
         if (errorMovimientos) {
             setAlert(true);
             setAlertOptions({ tipo: 'error', titulo: 'Error', mensaje: errorMovimientos })
         }
-
-        setMovimientos(movimientoData)
+            setMovimientos(movimientoData)
         
-    }
+    }, [errorMovimientos, fetchMovimientos]);
+
+
+    React.useEffect(() => {
+        //getMovimientos();
+    }, []) 
 
     return (
         <>
@@ -103,7 +125,7 @@ const Movimientos = () => {
                             Nuevo movimiento
                         </Button>
                        
-                        <Button startIcon={<CachedIcon />} variant="text" color='primary' onClick={refreshMovimientos}  disabled={loadingMovimientos}>
+                        <Button startIcon={<CachedIcon />} variant="text" color='primary' onClick={getMovimientos}  disabled={loadingMovimientos}>
                             Refrescar
                         </Button>
                     </Grid>
