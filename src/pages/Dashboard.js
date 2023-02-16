8a7535064e33e64bb467fe045aa871bc3a3d3d63
letiGaleano18 <dev02@digipar.com>
2023-02-16 14:27:28 -0300

    FIX borrar codigo no usable

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,14 +4,6 @@ import image from "../img/bienvenido.png";
 
 const Dashboard = () => {
   return (
-
-    // <background
-    //   sx={{ width: 500, height: 450 }}
-    //   src={`bienvenido.png`}
-    //   srcSet={`bienvenido.png`}
-    //   alt='Digipar'
-    //   loading="lazy"
-    // />
     <div style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat",backgroundSize:"contain",  height:1000,width:1000 }}>
 
   </div>
